1b51343414a2c0111f7a4cf527acb32c5793d41e
axdamx <axdamx@gmail.com>
2024-06-24 15:07:41 +0800

    Add a button to refetch

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,23 @@ import useGetPiQuery from "./hooks/useGetPiQuery";
 import useGetCircumferenceQuery from "./hooks/useGetCircumferenceQuery";
 
 function App() {
-  const { data: pi, isLoading: isLoadingPi, error: errorPi } = useGetPiQuery();
+  const {
+    data: pi,
+    isLoading: isLoadingPi,
+    error: errorPi,
+    refetch: refetchPi,
+  } = useGetPiQuery();
   const {
     data: circumference,
     isLoading: isLoadingCircumference,
     error: errorCircumference,
+    refetch: refetchCircumference,
   } = useGetCircumferenceQuery();
+
+  const handleRefresh = () => {
+    refetchPi();
+    refetchCircumference();
+  };
 
   if (isLoadingCircumference && isLoadingPi) {
     return <h1>Loading...</h1>;
@@ -30,6 +41,7 @@ function App() {
           <p>Current Value of Pi: {pi}</p>
           <p>Circumference of the Sun: {circumference} km</p>
         </div>
+        <button onClick={handleRefresh}>Refresh Data</button>
       </div>
     </>
   );
